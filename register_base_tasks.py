4b61209280e350cfbacbc4e9d4ecbb96afac9c97
Manigandan P <manigandan.p@avalara.com>
2024-09-29 11:40:31 +0530

    register new tasks for input and outputs

--- register_base_tasks.py
+++ register_base_tasks.py
@@ -37,23 +37,37 @@ def register_base_task(project_name, task_name, script_path, task_type):
 if __name__ == "__main__":
     import config
 
-    base_project_name = f"{config.base_project_name}/template"
+    base_project_template = f"{config.base_project_name}/template"
 
     register_base_task(
-        project_name=base_project_name,
+        project_name=base_project_template,
         task_name=config.download_dataset_base_task_name,
         script_path="dataset_loader.py",
         task_type=TaskTypes.data_processing,
     )
     register_base_task(
-        project_name=base_project_name,
+        project_name=base_project_template,
         task_name=config.transcribe_base_task_name,
         script_path="transcription_pipeline.py",
         task_type=TaskTypes.data_processing,
     )
     register_base_task(
-        project_name=base_project_name,
+        project_name=base_project_template,
         task_name=config.upload_dataset_base_task_name,
         script_path="dataset_upload.py",
         task_type=TaskTypes.data_processing,
+    )
+    
+    register_base_task(
+        project_name=base_project_template,
+        task_name=config.input_artifacts_task_name,
+        script_path=None,
+        task_type=TaskTypes.custom,
+    )
+    
+    register_base_task(
+        project_name=base_project_template,
+        task_name=config.output_artifacts_task_name,
+        script_path=None,
+        task_type=TaskTypes.custom,
     )
