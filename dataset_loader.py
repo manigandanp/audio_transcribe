319c63c668a8c586efcdff794f4a90809df7a55c
Manigandan P <manigandan.p@avalara.com>
2024-09-29 15:18:29 +0530

    update load_dataset logic

--- dataset_loader.py
+++ dataset_loader.py
@@ -17,15 +17,11 @@ class DatasetLoader:
 
     def upload_to_clearml(self):
 
-        dataset = (
-            load_dataset(
-                self.hf_dataset_name,
-                self.hf_config_name,
-                split="train",
-                token=self.token,
-            )
-            if self.hf_config_name
-            else load_dataset(self.hf_dataset_name, split="train", token=self.token)
+        dataset = load_dataset(
+            self.hf_dataset_name,
+            self.hf_config_name,
+            split="train",
+            token=self.token,
         )
         for item in dataset:
             filename = item["filename"]
@@ -46,12 +42,4 @@ if __name__ == "__main__":
     loader.upload_to_clearml()
 
     task.close()
-
-    # dataset_loader = DatasetLoader(
-    #     hf_dataset_name="mastermani305/ps-raw",
-    #     clearml_project="Test/dev/Audio Transcribe",
-    #     clearml_task_name="Dataset Task",
-    #     hf_config_name="ps-2-2-sample",
-    # )
-    # task_id = dataset_loader.upload_to_clearml()
-    # print(f"Task ID: {task_id}")
+    
