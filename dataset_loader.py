8d0c208ed6244092477edbd526f30de2fe7d7d9c
Manigandan P <manigandan.p@avalara.com>
2024-09-29 14:08:54 +0530

    fix dict getter issue

--- dataset_loader.py
+++ dataset_loader.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     task: Task = Task.current_task()
     task_parameters = task.get_parameters_as_dict()['General']
     input_task_id = task_parameters.get("input_task_id")
-    hf_dataset_name = task_parameters("hf_dataset_name")
-    hf_config_name = task_parameters("hf_config_name")
+    hf_dataset_name = task_parameters.get("hf_dataset_name")
+    hf_config_name = task_parameters.get("hf_config_name")
     print(f"Input Task ID: {input_task_id}", hf_output_dataset_name, hf_config_name)
     loader = DatasetLoader(input_task_id, hf_dataset_name, hf_config_name)
     loader.upload_to_clearml()
