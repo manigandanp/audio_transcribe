5e0254a1505d91ca4023ee96556b8c2cdff7b672
Manigandan P <manigandan.p@avalara.com>
2024-10-06 15:09:13 +0530

    minor fix

--- dataset_loader.py
+++ dataset_loader.py
@@ -43,4 +43,4 @@ if __name__ == "__main__":
     print("waiting for upload to complete")
     task.flush(wait_for_uploads=True)
     print("upload complete...")
-    task.close()
+    task.completed()
